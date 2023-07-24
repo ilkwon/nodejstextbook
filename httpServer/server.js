1c6b8ced1d4e9bc0b81e5a79e9d46a64bcba8c32
ilkwon <code666@gmail.com>
2023-07-24 13:48:46 +0900

    server1-1

--- httpServer/server.js
+++ httpServer/server.js
@@ -1,11 +1,17 @@
 const http = require('http');
 
 const PORT = 8080;
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
 	res.writeHead(200, { 'Content-Type':'text/html; charset=utf-8'});
 	res.write('<h1>Hello Node!</h1>');
 	res.end('<p>Hello Server!</p>')
-})
-	.listen(PORT, () => {
-		console.log(PORT +'번 포트에서 서버 대기 중입니다.');
-	});
+});
+server.listen(PORT);
+
+server.on('listening', () => {
+	console.log(PORT +'번 포트에서 서버 대기 중입니다.');
+});
+
+server.on('error', (err) => {
+	console.error(err);
+});
