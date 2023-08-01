0de2647671a0c4068df8026f5ff9fcec978be9d9
ilkwon <code666@gmail.com>
2023-08-01 21:30:26 +0900

    websocket tutorial 2

--- websocket/gif-chat/app.js
+++ websocket/gif-chat/app.js
@@ -7,4 +7,8 @@ const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 
 dotenv.config();
-const indexRouter = require('./routes')
+const indexRouter = require('./routes');
+
+const app = express();
+app.set('port', process.env.PORT || 8005);
+app.set('view engine', 'html')
